004b2f385ad1d4d4f99e2a7bb6982cf55555cba4
Nithiya <nmstreethran@gmail.com>
2019-04-19 18:10:46 +0200

    update docs, readme and data merging python files

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ by Nithiya Streethran (nmstreethran@gmail.com)
 
 This project was my dissertation for the Degree of MSc in Renewable Energy Engineering at Heriot-Watt University, submitted in August 2017. The files in this repository are modified and updated versions of my original submission.
 
-Updated files coming soon.
+Updated files coming soon. 
+
+## Licenses
+
+Unless otherwise specified:
+* Code: [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+* Documentation / content: [![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)
---
+++ downtime-merged.py
@@ -0,0 +1,53 @@
+#%% [markdown]
+# # Combining multiple csv files containing data into a single file
+
+#%% [markdown]
+# ## Downtime
+
+#%%
+import pandas as pd # import libraries
+
+#%%
+# import data as dataframes
+df_lsm=pd.read_csv('DATA/Last_six_months_downtime.csv',skip_blank_lines=True)
+df_pty=pd.read_csv('DATA/Prior_two_years_downtime.csv',skip_blank_lines=True)
+
+#%%
+# drop duplicate columns in data
+df_pty=df_pty.drop('AvailabilityCategory_id.1',axis=1)
+df_pty=df_pty.drop('EnvironmentalCategory_id.1',axis=1)
+df_pty=df_pty.drop('GridCategory_id.1',axis=1)
+df_pty=df_pty.drop('InfrastructureCategory_id.1',axis=1)
+df_pty=df_pty.drop('TurbineCategory_id.1',axis=1)
+df_pty=df_pty.drop('alarm_id.1',axis=1)
+df_pty=df_pty.drop('comment.1',axis=1)
+df_pty=df_pty.drop('id.1',axis=1)
+df_pty=df_pty.drop('workorder_id.1',axis=1)
+df_pty=df_pty.drop('turbine_id.1',axis=1)
+df_pty=df_pty.drop('timestamp_end.1',axis=1)
+df_pty=df_pty.drop('timestamp_start.1',axis=1)
+
+#%%
+# convert timestamps to datetime dtype
+df_lsm['timestamp_start']=pd.to_datetime(df_lsm['timestamp_start'])
+df_lsm['timestamp_end']=pd.to_datetime(df_lsm['timestamp_end'])
+df_pty['timestamp_start']=pd.to_datetime(df_pty['timestamp_start'])
+df_pty['timestamp_end']=pd.to_datetime(df_pty['timestamp_end'])
+
+#%%
+# concatenate both dataframes
+df_dwntm=pd.concat([df_lsm,df_pty]) 
+del df_lsm,df_pty
+
+#%%
+# sort by timestamp, then by turbine 
+df_dwntm=df_dwntm.sort_values(['timestamp_start','timestamp_end','turbine_id'])
+
+#%%
+# reset index after sort
+df_dwntm.reset_index(drop=True,inplace=True)
+
+#%%
+# save dataframe as new csv
+df_dwntm.to_csv('DATA/downtime_merged.csv',index=False)
+del df_dwntm
