950b6f1c701972fe95159f8cd9d0db9b7f68c5ed
nmstreethran <nmstreethran@gmail.com>
2019-12-03 08:03:57 +0100

    format scripts

--- SCADA-downtime-merged.py
+++ SCADA-downtime-merged.py
@@ -9,62 +9,69 @@ import pandas as pd # import libraries
 
 #%%
 # import downtime data
-df_dwntm=pd.read_csv('DATA/downtime_merged.csv',skip_blank_lines=True)
+df_dwntm = pd.read_csv('data/downtime_merged.csv',skip_blank_lines=True)
 
 #%%
-df_dwntm['timestamp_start']=pd.to_datetime(df_dwntm['timestamp_start']) # convert dtype object to datetime
-df_dwntm['timestamp_end']=pd.to_datetime(df_dwntm['timestamp_end'])
+df_dwntm['timestamp_start'] = pd.to_datetime(df_dwntm['timestamp_start']) # convert dtype object to datetime
+df_dwntm['timestamp_end'] = pd.to_datetime(df_dwntm['timestamp_end'])
 
 #%%
-df_dwntm['timestamp_start']=df_dwntm['timestamp_start'].dt.round('10min') # round to nearest 10 min
-df_dwntm['timestamp_end']=df_dwntm['timestamp_end'].dt.round('10min')
+df_dwntm['timestamp_start'] = df_dwntm['timestamp_start'].dt.round('10min') # round to nearest 10 min
+df_dwntm['timestamp_end'] = df_dwntm['timestamp_end'].dt.round('10min')
 
 #%%
-df_dwntm['period']=df_dwntm['timestamp_end']-df_dwntm['timestamp_start'] # calculate period
+df_dwntm['period'] = df_dwntm['timestamp_end'] - df_dwntm['timestamp_start'] # calculate period
 
 #%%
 # downtime ranges to every ten minutes between start and end timestamps
-df_dwntm=pd.concat([pd.DataFrame({'timestamp':pd.date_range(row.timestamp_start,row.timestamp_end,freq='10min'),
-                             'turbine_id':row.turbine_id,'period':row.period,'TurbineCategory_id':row.TurbineCategory_id,
-                             'EnvironmentalCategory_id':row.EnvironmentalCategory_id,
-                             'InfrastructureCategory_id':row.InfrastructureCategory_id,
-                             'GridCategory_id':row.GridCategory_id,
-                             'AvailabilityCategory_id':row.AvailabilityCategory_id,'alarm_id':row.alarm_id,
-                             'workorder_id':row.workorder_id,'comment':row.comment}, 
-                            columns=['timestamp','turbine_id','period','TurbineCategory_id','EnvironmentalCategory_id',
-                                     'InfrastructureCategory_id','GridCategory_id','AvailabilityCategory_id','alarm_id',
-                                     'workorder_id','comment']) 
-               for i,row in df_dwntm.iterrows()],ignore_index=True)
+df_dwntm = pd.concat([pd.DataFrame({
+    'timestamp': pd.date_range(
+        row.timestamp_start, row.timestamp_end, freq='10min'),
+    'turbine_id': row.turbine_id,
+    'period': row.period,
+    'TurbineCategory_id': row.TurbineCategory_id,
+    'EnvironmentalCategory_id': row.EnvironmentalCategory_id,
+    'InfrastructureCategory_id': row.InfrastructureCategory_id,
+    'GridCategory_id': row.GridCategory_id,
+    'AvailabilityCategory_id': row.AvailabilityCategory_id,
+    'alarm_id': row.alarm_id,
+    'workorder_id': row.workorder_id,
+    'comment': row.comment},
+    columns=['timestamp', 'turbine_id', 'period', 'TurbineCategory_id',
+        'EnvironmentalCategory_id', 'InfrastructureCategory_id',
+        'GridCategory_id', 'AvailabilityCategory_id', 'alarm_id',
+        'workorder_id','comment'])
+    for i, row in df_dwntm.iterrows()], ignore_index=True)
 
 #%%
-df_dwntm=df_dwntm.sort_values(['timestamp','turbine_id','period']) # sort and drop duplicates for same timestamp and turbine
-df_dwntm=df_dwntm.drop_duplicates(['timestamp','turbine_id'],keep='first')
+df_dwntm = df_dwntm.sort_values(['timestamp', 'turbine_id', 'period']) # sort and drop duplicates for same timestamp and turbine
+df_dwntm = df_dwntm.drop_duplicates(['timestamp', 'turbine_id'], keep='first')
 
 #%%
-df_scada=pd.read_csv('DATA/SCADA_merged.csv',skip_blank_lines=True) # import SCADA 
+df_scada = pd.read_csv('data/SCADA_merged.csv', skip_blank_lines=True) # import SCADA
 
 #%%
-df_scada=df_scada.drop('ws_1',axis=1) # drop unnecessary columns
-df_scada=df_scada.drop('ws_2',axis=1)
-df_scada=df_scada.drop('wd_1',axis=1)
-df_scada=df_scada.drop('wd_2',axis=1)
+df_scada = df_scada.drop('ws_1', axis=1) # drop unnecessary columns
+df_scada = df_scada.drop('ws_2', axis=1)
+df_scada = df_scada.drop('wd_1', axis=1)
+df_scada = df_scada.drop('wd_2', axis=1)
 
 #%%
-df_scada['timestamp']=pd.to_datetime(df_scada['timestamp'],dayfirst=True) # convert timestamp to datetime
+df_scada['timestamp'] = pd.to_datetime(df_scada['timestamp'], dayfirst=True) # convert timestamp to datetime
 
 #%%
-df_scada['turbine_id']=df_scada['turbine'] # copy turbine id to new column
+df_scada['turbine_id'] = df_scada['turbine'] # copy turbine id to new column
 
 #%%
-df_merged=pd.merge(df_scada,df_dwntm,how='outer') # merge SCADA and downtime
-del df_scada,df_dwntm
+df_merged = pd.merge(df_scada, df_dwntm, how='outer') # merge SCADA and downtime
+del df_scada, df_dwntm
 
 #%%
 # drop downtime entries with no SCADA readings - in case of duplicates
-df_merged=df_merged.drop(df_merged[(df_merged['turbine_id'].notnull())&(df_merged['turbine'].isnull())].index)
+df_merged = df_merged.drop(df_merged[(df_merged['turbine_id'].notnull()) & (df_merged['turbine'].isnull())].index)
 
 #%%
-df_merged=df_merged.drop('turbine',axis=1) # drop old turbine id column
+df_merged = df_merged.drop('turbine', axis=1) # drop old turbine id column
 
 #%%
 # list columns and their dtypes
@@ -75,34 +82,35 @@ list(df_merged.columns.values)
 
 #%%
 # rearrange columns
-df_merged=df_merged[['timestamp',
- 'turbine_id',
- 'ap_av',
- 'ap_dev',
- 'ap_max',
- 'reactive_power',
- 'ws_av',
- 'wd_av',
- 'gen_sp',
- 'nac_pos',
- 'pitch',
- 'rs_av',
- 'runtime',
- 'period',
- 'TurbineCategory_id',
- 'EnvironmentalCategory_id',
- 'InfrastructureCategory_id',
- 'GridCategory_id',
- 'AvailabilityCategory_id',
- 'alarm_id',
- 'workorder_id',
- 'comment']]
+df_merged = df_merged[[
+    'timestamp',
+    'turbine_id',
+    'ap_av',
+    'ap_dev',
+    'ap_max',
+    'reactive_power',
+    'ws_av',
+    'wd_av',
+    'gen_sp',
+    'nac_pos',
+    'pitch',
+    'rs_av',
+    'runtime',
+    'period',
+    'TurbineCategory_id',
+    'EnvironmentalCategory_id',
+    'InfrastructureCategory_id',
+    'GridCategory_id',
+    'AvailabilityCategory_id',
+    'alarm_id',
+    'workorder_id',
+    'comment']]
 
 #%%
 # write final dataframe to csv
-df_merged.to_csv('DATA/SCADA_downtime_merged.csv',index=False)
+df_merged.to_csv('data/SCADA_downtime_merged.csv', index=False)
 
 #%%
 # importing the new csv as a dataframe (encoding needs to be specified)
 # import pandas as pd
-# dfnew=pd.read_csv('DATA/SCADA_downtime_merged.csv',encoding="ISO-8859-1")
+# dfnew = pd.read_csv('data/SCADA_downtime_merged.csv', encoding="ISO-8859-1")
--- downtime-categories.py
+++ downtime-categories.py
@@ -2,51 +2,43 @@
 # # Downtime frequency and period based on turbine category
 
 #%%
+# import libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-df=pd.read_csv('path/turbine_categories.csv')
-df['Turbine Category id']=df['Turbine Category id'].astype(np.float64)
-df=df.sort_values(by="freq/T/yr")
-df['number']=range(0,23)
-df.reset_index(drop=True,inplace=True)
-df
-
 
 #%%
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
-df=pd.read_csv('path/turbine_categories.csv')
-df['Turbine Category id']=df['Turbine Category id'].astype(np.float64)
-df=df.sort_values(by="freq/T/yr")
-df['number']=range(0,23)
-df.reset_index(drop=True,inplace=True)
+df = pd.read_csv('data/turbine_categories.csv')
+df['Turbine Category id'] = df['Turbine Category id'].astype(np.float64)
+df = df.sort_values(by="freq/T/yr")
+df['number'] = range(0, 23)
+df.reset_index(drop=True, inplace=True)
 
-y=df['number']
-x1=df['freq/T/yr']
-x2=df['period/T/yr']
-list1=df['Turbine Categories'].tolist()
+#%%
+y = df['number']
+x1 = df['freq/T/yr']
+x2 = df['period/T/yr']
+list1 = df['Turbine Categories'].tolist()
 
-fig,axes=plt.subplots(ncols=2,sharey=True,figsize=(14,7),dpi=500)
-plt.suptitle('Turbine category',x=.513,y=.9,fontsize=12)
-axes[0].barh(y,x1,align='center',color='#098A63')
-axes[0].set_xlabel('Downtime frequency per turbine per year',fontsize=12)
-axes[1].barh(y,x2,align='center',color='#3F2B78')
-axes[1].set_xlabel('Downtime period (hours) per turbine per year',fontsize=12)
+#%%
+fig, axes = plt.subplots(ncols=2, sharey=True, figsize=(14, 7), dpi=500)
+plt.suptitle('Turbine category', x=.513, y=.9, fontsize=12)
+axes[0].barh(y, x1, align='center', color='#098A63')
+axes[0].set_xlabel('Downtime frequency per turbine per year', fontsize=12)
+axes[1].barh(y, x2, align='center', color='#3F2B78')
+axes[1].set_xlabel('Downtime period (hours) per turbine per year', fontsize=12)
 
 axes[0].invert_xaxis()
-axes[0].set(yticks=y,yticklabels=list1)
+axes[0].set(yticks=y, yticklabels=list1)
 
-axes[0].set(yticks=y,yticklabels=[])
-for yloc,list1 in zip(y,list1):
-    axes[0].annotate(list1,(.5,yloc),xycoords=('figure fraction','data'),
-                     ha='center',va='center')
+axes[0].set(yticks=y, yticklabels=[])
+for yloc, list1 in zip(y, list1):
+    axes[0].annotate(list1, (.5, yloc), xycoords=('figure fraction', 'data'),
+                     ha='center', va='center')
 
 axes[0].yaxis.tick_right()
 
-fig.subplots_adjust(wspace=.4,hspace=None)
+fig.subplots_adjust(wspace=.4, hspace=None)
 
 plt.show()
