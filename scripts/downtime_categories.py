3577194bce069d97468c592fea5a836deffaebb1
Nithiya Streethran <nmstreethran@gmail.com>
2022-04-13 23:39:51 +0100

    update nb, scripts

---
+++ scripts/downtime_categories.py
@@ -0,0 +1,35 @@
+"""Downtime categories
+
+Generating turbine downtime statistics
+"""
+
+# import libraries
+import pandas as pd
+
+data_cat = pd.read_csv("data/processed/downtime_categories.csv")
+data_ts = pd.read_csv(
+    "data/processed/downtime_timeseries.csv",
+    lineterminator="\n",
+    parse_dates=["timestamp_start", "timestamp_end"]
+)
+
+data_ts["period"] = data_ts["timestamp_end"] - data_ts["timestamp_start"]
+data_ts["period"] = data_ts["period"].dt.total_seconds() / (60 * 60)
+
+data = pd.DataFrame(data_ts.groupby(["TurbineCategory_id"]).count()["id"])
+data.rename(columns={"id": "frequency"}, inplace=True)
+data["Category"] = data.index
+
+data["period"] = data_ts.groupby(["TurbineCategory_id"]).sum()["period"]
+
+data["frequency/t/y"] = data["frequency"] / (25 * 2.5)
+data["period/t/y"] = data["period"] / (25 * 2.5)
+
+data_cat = data_cat[data_cat["Type"] == "Turbine"]
+data_cat = data_cat.drop(columns=["Type"])
+
+data = pd.concat([data, data_cat], axis=1)
+data.fillna(0, inplace=True)
+data.sort_values(by="frequency", inplace=True)
+
+data.to_csv("data/processed/downtime_categories_stats.csv")
--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,10 +1,8 @@
-# %% [markdown]
-# # Process data
+"""Process data
 
-# %% [markdown]
-# ## Downtime categories
+Processing and merging datasets
+"""
 
-# %%
 # import libraries
 import os
 import glob
@@ -12,19 +10,16 @@ import itertools
 import pandas as pd
 import numpy as np
 
-# %%
 # create directory to store processed data
 os.makedirs("data/processed/", exist_ok=True)
 
-# %%
+# Downtime categories
 # read and view data
 data = pd.read_excel("data/Melogale Downtime Categories.xlsx")
 
-# %%
 # drop first row
 data = data.drop([0])
 
-# %%
 # function to filter data for each category type
 catData = {}
 
@@ -43,7 +38,6 @@ def categorise_data(cat, number):
     catData[cat].dropna(inplace=True)
 
 
-# %%
 # filtering
 categorise_data("Turbine", 1)
 categorise_data("Environmental", 3)
@@ -51,56 +45,42 @@ categorise_data("Grid", 5)
 categorise_data("Infrastructure", 7)
 categorise_data("Availability", 9)
 
-# %%
 # concatenate data
 data = pd.concat(catData.values(), ignore_index=True)
 
-# %%
 # save data as CSV
 data.to_csv("data/processed/downtime_categories.csv", index=False)
 
-# %% [markdown]
-# ## Downtime time series
-
-# %%
+# Downtime time series
 dt = {}
 dtList = glob.glob("data/*downtime*.csv")
 for num, df in enumerate(dtList):
     dt[num] = pd.read_csv(df)
     print(num, df, dt[num].shape)
 
-# %%
 # drop duplicate columns
 dt[1].drop(columns=list(dt[1].filter(regex=".1")), inplace=True)
 
-# %%
 # convert timestamps to datetime data type
 for key in dt.keys():
     for col in list(dt[key].filter(regex="timestamp")):
         dt[key][col] = pd.to_datetime(dt[key][col])
 
-# %%
 # concatenate data
 data = pd.concat(dt.values(), join="outer")
 
-# %%
 # sort by timestamp, then by turbine
 data = data.sort_values(["timestamp_start", "timestamp_end", "turbine_id"])
 
-# %%
 # reset index after sort
 data.reset_index(drop=True, inplace=True)
 
-# %%
 # save dataframe as new CSV
 data.to_csv(
     "data/processed/downtime_timeseries.csv", index=False, encoding="utf-8"
 )
 
-# %% [markdown]
-# ## SCADA time series
-
-# %%
+# SCADA time series
 # old SCADA data
 scada = {}
 scadaList = glob.glob("data/*SCADA.csv")
@@ -108,28 +88,23 @@ for num, df in enumerate(scadaList):
     scada[num] = pd.read_csv(df)
     print(num, df, scada[num].shape)
 
-# %%
 # rename ap_min in scada[1] to ap_max
 scada[1].rename(columns={"ap_min": "ap_max"}, inplace=True)
 
 
-# %%
 # fixing rotor speed readings due to errors in data
 def fix_rs(c):
     if c["turbine"] <= 20:
         return c["rs_av"]
 
 
-# %%
 for df in scada.keys():
     scada[df]["rs_av_old"] = scada[df].apply(fix_rs, axis=1)
     scada[df] = scada[df].drop("rs_av", axis=1)
 
-# %%
 # concatenate old datasets
 scadaOld = pd.concat(scada.values())
 
-# %%
 # new SCADA data
 scada = {}
 scadaList = glob.glob("data/NS_SCADA*.csv")
@@ -137,28 +112,23 @@ for num, df in enumerate(scadaList):
     scada[num] = pd.read_csv(df)
     print(num, df, scada[num].shape)
 
-# %%
 # concatenate new datasets
 scadaNew = pd.concat(scada.values())
 
-# %%
 # convert timestamp to datetime data type
 scadaNew["timestamp"] = pd.to_datetime(scadaNew["timestamp"])
 scadaOld["timestamp"] = pd.to_datetime(scadaOld["timestamp"], dayfirst=True)
 
-# %%
 # rename columns
 scadaNew.rename(columns={"turbine_id": "turbine"}, inplace=True)
 scadaNew.rename(columns={"rs_av": "rs_av_new"}, inplace=True)
 
-# %%
 # sort values and drop duplicates
 scadaNew = scadaNew.sort_values(["timestamp", "turbine"])
 scadaOld = scadaOld.sort_values(["timestamp", "turbine"])
 scadaNew = scadaNew.drop_duplicates(["timestamp", "turbine"], keep="first")
 scadaOld = scadaOld.drop_duplicates(["timestamp", "turbine"], keep="first")
 
-# %%
 # fill missing rows in time series
 tmstmp = list(pd.date_range(
     "2014-11-01 00:00:00", "2017-04-30 23:50:00", freq="10min"
@@ -175,7 +145,6 @@ scadaOld_cols = pd.merge(
     scadaOld, cols, on=["timestamp", "turbine"], how="outer"
 )
 
-# %%
 # merge old and new data by these columns
 scada = pd.merge(scadaOld_cols, scadaNew, on=[
     "timestamp", "turbine", "ws_av", "wd_av", "ws_1", "ws_2", "wd_1",
@@ -183,13 +152,11 @@ scada = pd.merge(scadaOld_cols, scadaNew, on=[
     "ap_av", "nac_pos"
 ], how="outer")
 
-# %%
 # sort and drop duplicates again
 scada = scada.sort_values(["timestamp", "turbine"])
 scada = scada.drop_duplicates(["timestamp", "turbine"], keep="first")
 
 
-# %%
 # merge rotor speed readings
 def merge_rotor_readings(c):
     if c["rs_av_new"] >= 0:
@@ -200,12 +167,10 @@ def merge_rotor_readings(c):
 
 scada["rs_av"] = scada.apply(merge_rotor_readings, axis=1)
 
-# %%
 # drop old columns and reset index
 scada = scada.drop("rs_av_old", axis=1)
 scada = scada.drop("rs_av_new", axis=1)
 scada.reset_index(drop=True, inplace=True)
 
-# %%
 # write to new CSV file
 scada.to_csv("data/processed/SCADA_timeseries.csv", index=False)
