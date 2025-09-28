0ba4e326a9fa6b6405e678b60c59a70e1599ee79
Nithiya Streethran <nmstreethran@gmail.com>
2025-09-28 14:57:53 +0100

    add docs structure

--- docs/notebooks/conf.py
+++ docs/notebooks/conf.py
@@ -7,13 +7,26 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'WindTurbineClassification'
-copyright = '2025, Nithiya Streethran'
+copyright = "2017-2025, Nithiya Streethran"
 author = 'Nithiya Streethran'
+
+github_url = f"https://github.com/nmstreethran/{project}"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_nb"]
+
+# disable execution of Jupyter notebooks
+nb_execution_mode = "off"
+
+viewcode_line_numbers = True
+
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +36,25 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_title = project
+
+html_theme = "pydata_sphinx_theme"
+
 html_static_path = ['_static']
+
+html_theme_options = {
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": github_url,  # required
+            # Icon class (if 'type': 'fontawesome'), or path to local image
+            # (if 'type': 'local')
+            "icon": "fa-brands fa-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+    ],
+    "navbar_align": "right",
+}
--- docs/notebooks/index.rst
+++ docs/notebooks/index.rst
@@ -3,15 +3,16 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-WindTurbineClassification documentation
-=======================================
+WindTurbineClassification
+=========================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+Specification of 'normal' wind turbine operating behaviour for rapid anomaly detection: through the use of machine learning algorithms
 
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+
+   report
+   notebooks
+
 
